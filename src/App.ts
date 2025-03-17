b98422fcbe80a62b25244b4c910cc4ca541a81ca
Léa BAR <contact@0xlea.fr>
2025-03-17 10:21:02 +0100

    [Add] Add App and AppWIndow and scripts to build and start

---
+++ src/App.ts
@@ -0,0 +1,4 @@
+import { app } from 'electron';
+import AppWindow from './AppWindow';
+
+AppWindow.main(app)
---
+++ src/AppWindow.ts
@@ -0,0 +1,31 @@
+import { BrowserWindow, App } from 'electron';
+
+export default class AppWindow {
+    private static mainWindow: BrowserWindow | null = null;
+    private static application: App;
+
+    private static onWindowAllClosed = () => {
+        if (process.platform !== 'darwin') {
+            AppWindow.application.quit();
+        }
+    };
+
+    private static onClose = () => {
+        AppWindow.mainWindow = null;
+    };
+
+    private static onReady = () => {
+        AppWindow.mainWindow = new BrowserWindow({
+            width: 1080,
+            height: 720,
+            frame: false
+        });
+        AppWindow.mainWindow.on('closed', AppWindow.onClose);
+    };
+
+    static main(app: App) {
+        AppWindow.application = app;
+        app.on('window-all-closed', AppWindow.onWindowAllClosed);
+        app.once('ready', AppWindow.onReady);
+    }
+}
